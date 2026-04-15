7eef94aabafec016bebb4f641dfcaf3336339214
Kaim Arvas <145204148+Skaimarvas@users.noreply.github.com>
2026-04-15 14:13:21 +0300

    refactor: remove OAuth buttons from signup form and clean up sidebar layout
    - Removed Google and X signup buttons from the signup form component.
    - Cleaned up the app sidebar component by removing the support and others sections, along with their associated logic and items.
    - Updated sidebar navigation items to reflect new paths and names for inventory and sales management.
    - Adjusted the auth page layout by simplifying the logo section.

--- src/app/pages/user-management/user-management.component.ts
+++ src/app/pages/user-management/user-management.component.ts
@@ -1,15 +1,10 @@
-
 import { Component } from '@angular/core';
 import { PageBreadcrumbComponent } from '../../shared/components/common/page-breadcrumb/page-breadcrumb.component';
 
 @Component({
-  selector: 'app-blank',
-  imports: [
-    PageBreadcrumbComponent
-],
-  templateUrl: './blank.component.html',
+  selector: 'app-user-management',
+  imports: [PageBreadcrumbComponent],
+  templateUrl: './user-management.component.html',
   styles: ``
 })
-export class BlankComponent {
-
-}
+export class UserManagementComponent {}
